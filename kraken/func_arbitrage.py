dee53c932bcc0ca8b6cccee84b4ca9903ccfecc1
mcreekmore <matthewacreekmore@gmail.com>
2023-01-05 15:28:33 -0500

    calculated triangular pairs

---
+++ kraken/func_arbitrage.py
@@ -0,0 +1,59 @@
+def get_tradeable_pairs(pairs):
+    tradeable_pairs = []
+
+    for pair in pairs:
+        # add pair name to object
+        pairs[pair]["pair_name"] = pair
+
+        # only allows 'online' pairs
+        if pairs[pair]['status'] == 'online':
+            tradeable_pairs.append(pairs[pair])
+
+    return tradeable_pairs
+
+
+def structure_arbitrage_pairs(pairs):
+    triangular_pairs_list = []
+    remove_duplicates_list = []
+    pairs_list = pairs[:10]
+
+    # get pair a
+    for pair_a in pairs:
+        a_base = pair_a['base']
+        a_quote = pair_a['quote']
+        # assign a to a box
+        a_pair_box = [a_base, a_quote]
+
+        # get pair b
+        for pair_b in pairs:
+            b_base = pair_b['base']
+            b_quote = pair_b['quote']
+            b_pair_box = [b_base, b_quote]
+
+            # check pair b
+            if pair_b != pair_a:
+                if b_base in a_pair_box or b_quote in a_pair_box:
+
+                    # get pair c
+                    for pair_c in pairs:
+                        c_base = pair_c['base']
+                        c_quote = pair_c['quote']
+                        c_pair_box = [c_base, c_quote]
+
+                        # count number of matching c items
+                        if pair_c != pair_a and pair_c != pair_b:
+                            combine_all = [pair_a, pair_b, pair_c]
+                            pair_box = [a_base, a_quote, b_base,
+                                        b_quote, c_base, c_quote]
+
+                            counts_c_base = 0
+                            counts_c_quote = 0
+                            for i in pair_box:
+                                if i == c_base:
+                                    counts_c_base += 1
+                                if i == c_quote:
+                                    counts_c_quote += 1
+
+                            # determine triangular match
+                            if counts_c_base == 2 and counts_c_quote == 2 and c_base != c_quote:
+                                print(a_pair_box, b_pair_box, c_pair_box)
--- kraken/main.py
+++ kraken/main.py
@@ -1,13 +1,26 @@
 import requests
+import func_arbitrage
 
-resp = requests.get(
-    'https://api.kraken.com/0/public/AssetPairs')
 
-asset_pairs = resp.json()
+# Step 0: Get list of tradeable pairs
+def step_0():
+    # get asset pairs
+    res = requests.get(
+        'https://api.kraken.com/0/public/AssetPairs').json()
 
-for pair in asset_pairs['result']:
-    if 'XMR' in pair:
-        print(asset_pairs['result'][pair]['base'] +
-              '/' + asset_pairs['result'][pair]['quote'])
-        print(pair)
-        print(asset_pairs['result'][pair])
+    asset_pairs = res['result']
+
+    # get tradeable pairs
+    tradeable_pairs = func_arbitrage.get_tradeable_pairs(asset_pairs)
+
+    return tradeable_pairs
+
+
+# Step 1: Structuring Triangular Pairs
+def step_1(pairs):
+    structured_list = func_arbitrage.structure_arbitrage_pairs(pairs)
+
+
+if __name__ == "__main__":
+    tradeable_pairs = step_0()
+    step_1(tradeable_pairs)
