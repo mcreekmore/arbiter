bc60e82e1fcacd6239055360af720aa0e4db1fe5
Matt Creekmore <matthewacreekmore@gmail.com>
2023-01-24 12:45:25 -0500

    complete flash loan v3 structure

--- flash-loan/hardhat.config.ts
+++ flash-loan/hardhat.config.ts
@@ -5,8 +5,11 @@ dotenv.config()
 
 require('dotenv').config({ path: '../.env' })
 
+// deployed address: 0xB18bB0220e2Af838cb4f7f1AB6dEA70882c4203a
+// goerli USDC: 0x07865c6E87B9F70255377e024ace6630C1Eaa37F DON'T USE THIS
+
 const config: HardhatUserConfig = {
-  solidity: '0.8.17',
+  solidity: '0.8.10',
   networks: {
     goerli: {
       url:
